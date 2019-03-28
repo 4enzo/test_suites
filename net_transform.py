9eac2a4639a3eb57dc3e60581b9aa5c90cfbb68e
4enzo <archer.enzo@hotmail.com>
2019-03-28 23:12:57 +0800

    实现由位数获取掩码功能，如8———>255.0.0.0

--- net_transform.py
+++ net_transform.py
@@ -16,13 +16,23 @@ def mask2length():
     print('\n掩码 %s 对应的长度为：%d\n'%(mask,len(r_bin.replace('0',''))))
 
 def length2mask():
-    pass
+    #由位数获取掩码，如8————>255.0.0.0
+    length = int(input('请输入位数：'))
+    res_bin = '1'*length + '0'*(32-length)
+
+    a = int(res_bin[0:8],2)
+    b = int(res_bin[8:16],2)
+    c = int(res_bin[16:24],2)
+    d = int(res_bin[24:32],2)
+
+    r_int = str(a) + '.' + str(b) + '.' + str(c) + '.' + str(d)
+    print('\n位数 %d 对应的掩码为：%s\n'%(length,r_int))
 
 def main():
     while True:
         print('*'*20 + '网络转换小工具' + '*'*5 + 'by Enzo' + '*'*20)
         print('1:掩码转位数')
-        print('2:位数转掩码（未实现）')
+        print('2:位数转掩码')
 
         print('0:退出')
 
@@ -33,6 +43,9 @@ def main():
             break
         elif fun_num == 1:
             mask2length()
+            continue
+        elif fun_num == 2:
+            length2mask()
             continue
         else:
             print('error/功能未实现')
