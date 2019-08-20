12cbd023948e6bfd03ef64280291d6546899a371
4enzo <archer.enzo@hotmail.com>
2019-08-20 23:02:41 +0800

    先将ip归属地等信息拉下来

--- analysis_hosts_deny.py
+++ analysis_hosts_deny.py
@@ -5,8 +5,11 @@ Created on 2019-08-19
 @author: Enzo
 '''
 
+import time
 import json
 import requests
+
+import pymongo
 
 
 #vps天天被爆破，故安装DenyHosts,当某个ip尝试登陆三次失败后，将此ip加入hosts.deny，以屏蔽该ip
@@ -17,31 +20,72 @@ import requests
 
 
 def get_hosts_deny():
+    hosts_list = []
     file_hosts_deny = '.\hosts.deny'
     with open(file_hosts_deny) as f:
         for line in f.readlines():
             if "ALL: " == line[:5]:
-                print("The deny host is %s"%line[5:])
+                hosts_list.append(line[5:].replace("\n",""))
+                # print("The deny host is %s"%line[5:])
+        return hosts_list
 
-
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.79 Safari/537.36"
-}
-
-
-def get_hosts_locaion(ip):
+def get_hosts_location(ip):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.79 Safari/537.36"
+    }
     # url = "http://ip.taobao.com/service/getIpInfo.php?ip=" + ip
     url = "http://ip-api.com/json/" + ip + "?lang=zh-CN"
     result = requests.get(url,headers=headers)
     if result.status_code == 200:
         content = json.loads(result.text)
         if content["status"] == "success":
-            print(content)
+            # print(content)
             return content
     else:
         print("error:result.status_code:%s"%result.status_code)
 
+def write2file(location):
+    with open('.\location.txt','a',encoding='utf-8') as f:
+        f.write(json.dumps(location)+'\n')
+
+def load_file():
+    location_list = []
+    with open('.\location.txt','r',encoding='utf-8') as f:
+        for line in f.readlines():
+            location_list.append(json.loads(line))
+            # print(json.loads(line))
+
+    return location_list
+
+def write2db(location_info):
+
+    conn = pymongo.MongoClient('192.168.123.100',27017)
+    # 连接mydata数据库，没有则自动创建
+    db = conn.mydata
+    # 使用hosts_deny集合，没有则自动创建
+    my_collection = db.hosts_deny
+    if my_collection.find_one({"query":location_info["query"]}):
+        print("已存在")
+        pass
+    else:
+        my_collection.insert(location_info)
+        print("插入数据库")
+
+    conn.close()
+
+
+def main():
+    for ip in get_hosts_deny():
+        a = get_hosts_location(ip)
+    #     # write2db(a)
+        write2file(a)
+        time.sleep(0.5)
+    # for line in load_file():
+        # print(line)
+    # for line in load_file():
+    #     print(line)
+
+
 
 if __name__ == '__main__':
-    # get_hosts_deny()
-    get_hosts_locaion("1.2.3.4")
+    main()
