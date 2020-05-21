d71f030ec5c43947e04ad39cf6456b32a72ad081
4enzo <archer.enzo@hotmail.com>
2020-05-21 14:39:01 +0800

    每日停止-sleep-启动docker container解决大量无用连接 ---基于container name进行操作

--- restart_container.py
+++ restart_container.py
@@ -9,27 +9,31 @@ import subprocess
 import time
 import datetime
 
-def start_container():
-    cmd = "docker run -d xxxxxx"
-    status, container_id = subprocess.getstatusoutput(cmd)
-    return container_id
-
-def stop_rm_container(id):
-    cmd1 = "docker stop %s" % id
-    status1, results = subprocess.getstatusoutput(cmd1)
-    cmd2 = "docker rm %s" % id
-    status2, results = subprocess.getstatusoutput(cmd2)
-    if status1 == 0 and status2 == 0:
+def start_container(container_name):
+    cmd = "docker run -d -p 443:443 --name %s --restart=always xxxxxx"%container_name
+    status, result = subprocess.getstatusoutput(cmd)
+    if status == 0:
         return True
     else:
+        print("Error:%s"%result)
+        return False
+
+def stop_rm_container(container_name):
+    cmd = "docker stop %s && docker rm %s" % (container_name,container_name)
+    status, result = subprocess.getstatusoutput(cmd)
+    if status == 0:
+        return True
+    else:
+        print("Error:%s"%result)
         return False
 
 if __name__ == '__main__':
-    container_id = start_container()
-    while True:
-        if datetime.datetime.now().hour == 4:
-            stop_rm_container(container_id)
-            time.sleep(3600)
-            container_id = start_container()
-        else:
-            time.sleep(600)
+    container_name = "container_name"
+    if start_container(container_name):
+        while True:
+            if datetime.datetime.now().hour == 4:
+                stop_rm_container(container_name)
+                time.sleep(3600)
+                start_container(container_name)
+            else:
+                time.sleep(600)
